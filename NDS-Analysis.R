eb9ba2e2525b8f139b24f70f777cdb2d9b0c503c
Sarah-Roley <sarah.roley@wsu.edu>
2018-07-27 10:05:30 -0700

    added greenhouse v lab figures 26 July 2018

--- NDS-Analysis.R
+++ NDS-Analysis.R
@@ -1,4 +1,14 @@
 #2-Jul-18
 #Code to analyze NDS experiment in Yakima Basin
-#Sarah ROley, Clay Arango, Alex Alexiades
+#Sarah Roley, Clay Arango, Alex Alexiades
 
+#Columbia River: Greenhouse vs. lab boxplots
+library(ggplot2)
+
+GH_lab<-read.csv("ColumbiaRiver_GH_lab.csv")
+
+GH_lab_noC<-subset(GH_lab, !nutrient=="control")
+ggplot(GH_lab_noC, aes(x=nutrient, y= cr.nrr))+geom_boxplot(aes(color=factor(incubation)))+facet_wrap(~top)+theme_bw()+
+  geom_hline(yintercept=1)
+ggplot(GH_lab_noC, aes(x=nutrient, y= gpp.nrr))+geom_boxplot(aes(color=factor(incubation)))+facet_wrap(~top)+theme_bw()+
+  geom_hline(yintercept=1)
