4975aa00027ad0b75e663ef02709fb1179199194
clayarango <cletus22@gmail.com>
2019-02-14 17:34:15 -0800

    upper yak analysis

--- Wen-Fall.R
+++ Wen-Fall.R
@@ -17,12 +17,10 @@ library(MASS)
 library(ggplot2)
 
 #Load data
-roza_summer <- read.table(file="roza_summer.csv", header=T, sep=",")
+wen_fall <- read.table(file="wen_fall.csv", header=T, sep=",")
 
 #set variable
-d = roza_summer
-
-#new thing
+d = wen_fall
 
 #evaluate data to make sure factors are correct
 names(d)
@@ -48,12 +46,12 @@ d.gpp = subset(d, top=="glass", data=d)
 #calculate nrr for cr
 x = aggregate(d.cr[,8], list(d.cr$nutrient), mean, na.rm=T)
 x
-d.cr$cr.nrr = d.cr$cr.area/-9.818856 #is there a prettier way to do this?
+d.cr$cr.nrr = d.cr$cr.area/-3.997597 #is there a prettier way to do this?
 
 #calculate nrr for gpp
 x = aggregate(d.gpp[,9], list(d.gpp$nutrient), mean, na.rm=T)
 x
-d.gpp$gpp.nrr = d.gpp$gpp.area/4.059326 #is there a prettier way to do this?
+d.gpp$gpp.nrr = d.gpp$gpp.area/1.8995916 #is there a prettier way to do this?
 
 ############################################################
 #analyze RESPIRATION data
@@ -64,17 +62,17 @@ E1<-residuals(M1)
 qqnorm(E1)
 qqline(E1)
 ad.test(E1)
-   #residuals are normally distributed, p=0.5626
+   #residuals are ok
 hist(E1)  
 plot(M1)
 
 plot(filter(d.cr, !is.na(cr.area)) %>% dplyr::select(nutrient), 
      E1, xlab="nutrient", ylab="Residuals")
 bartlett.test(cr.area~nutrient, data=d.cr)
-   #variance test OK
+   #variance test is ok
 
 anova(M1)
-  #co-limited by N, P, and Si
+  #co-limited by P and Si
 
 x <- group_by(d.cr, nutrient) %>%  # Grouping function causes subsequent functions to aggregate by season and reach
   summarize(cr.mean = abs(mean(cr.area, na.rm = TRUE)), # na.rm = TRUE to remove missing values
@@ -104,7 +102,7 @@ ggplot(data=x, aes(x=nutrient, y=cr.mean)) +
         axis.title.x=element_text(size=8), 
         axis.text.x=element_text(size=8))
 
-#ggsave('output/figures/Roza_summer.tiff',
+#ggsave('output/figures/Wen_fall.tiff',
 #       units="in",
 #       width=3.25,
 #       height=3.25,
@@ -131,7 +129,7 @@ bartlett.test(gpp.area~nutrient, data=d.gpp)
    #OK
 
 anova(M1)
-  #P+Si co-limitation
+  #no limitation
 
 x <- group_by(d.gpp, nutrient) %>%  # Grouping function causes subsequent functions to aggregate by season and reach
   summarize(gpp.mean = abs(mean(gpp.area, na.rm = TRUE)), # na.rm = TRUE to remove missing values
