ba674d8cc48b004d095e3b7e606e4e414db6c2ab
Sarah-Roley <sarah.roley@wsu.edu>
2019-05-17 12:56:35 -0700

    updated

--- water_chem.R
+++ water_chem.R
@@ -15,13 +15,23 @@ str(DIN)
 
 #calculate DIN
 DIN$DIN_mgNL<-DIN$NH4_mgNL+DIN$NO3_mgNL
-DIN$type<-recode(DIN$Site, "'YAK'='main'; 'Selah'='main', 'Cle Elum'='main'; 'Ellensburg'='main'; 'Main Stem 3'='main'; 'Main Stem 1' ='main';
-                 'Main Stem 2' = 'main'; 'Ringer'='main'")
 
-#calculate nrr for gpp and chla
-N_ave<- ddply(DIN, c("Site_Full_Name", "Time"), summarise, NO3_ave = mean(NO3_mgNL), NH4_ave = mean(NH4_mgNL), DIN_ave = mean(DIN_mgNL)) 
+#averages
+N_ave<- ddply(DIN, c("Site_Full_Name", "Time", "type"), summarise, NO3_ave = mean(NO3_mgNL), NH4_ave = mean(NH4_mgNL), DIN_ave = mean(DIN_mgNL)) 
 N_ave
 
 write.table(N_ave, "DIN_summary.csv", sep=",", quote=F, row.names=F)
 
-ggplot(N_ave, aes(x=Site_full))
+#did some Excel data manipulation for stacked bar...
+DIN_stackedbar<-read.csv("DIN_stackedbar.csv")
+
+#plot
+DIN_stackedbar$Site_Full_Name<-factor(DIN_stackedbar$Site_Full_Name,levels= c("Reecer", "Wenas", "Ahtanum", "Toppenish", "Satus")) 
+DIN_stackedbar$Time<-factor(DIN_stackedbar$Time,levels= c("summer", "fall")) 
+
+ggplot(subset(DIN_stackedbar, type=="trib"), aes(fill=DIN_type, x=Site_Full_Name, y=N_mgL))+geom_col()+theme_classic()+
+  theme(axis.title.x =element_blank())+ylab("DIN (mg N/L)")+scale_fill_manual(values = c("springgreen4","cyan4"))+
+  facet_grid(~Time)+theme(axis.text.x=element_text(size = 12, angle =20), axis.text.y = element_text(size=12),
+                                  axis.title.y = element_text(size=14), legend.position="none")
+
+                                                                              
